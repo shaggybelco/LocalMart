30b5dc00bfa44580452226b6a2d175537e965dc9
Shaggy Sambo <shaggy@digitalsolutionfoundry.co.za>
2026-05-30 14:01:05 +0200

    Initial commit
    Generated by create-expo-app 4.0.0.

---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBfCY6q72nakyauDc5L8dqWy_DyeXRoKI",
+  authDomain: "localmart-cfc80.firebaseapp.com",
+  projectId: "localmart-cfc80",
+  storageBucket: "localmart-cfc80.firebasestorage.app",
+  messagingSenderId: "546227077958",
+  appId: "1:546227077958:web:249b55b5bea70f3ccb9b47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
